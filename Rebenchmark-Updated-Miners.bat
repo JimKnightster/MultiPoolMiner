375e734f769b23b905e7ed0e116011b7e3ec9b01
Grant Emsley <grant@emsley.ca>
2018-01-27 15:19:02 -0500

    New rebenchmarking reset script

--- Rebenchmark-Updated-Miners.bat
+++ Rebenchmark-Updated-Miners.bat
@@ -2,12 +2,10 @@
 cd /d %~dp0
 ECHO Deleting benchmark data for updated miners
 
-if exist "Stats\MkxminerAmd_*_HashRate.txt" del "Stats\MkxminerAmd_*_HashRate.txt"
-if exist "Stats\BMiner_*_HashRate.txt" del "Stats\BMiner_*_HashRate.txt"
-if exist "Stats\EMiner_*_HashRate.txt" del "Stats\EMiner_*_HashRate.txt"
 if exist "Stats\Cast_*_HashRate.txt" del "Stats\Cast_*_HashRate.txt"
-if exist "Stats\EthminerNvidia_*_HashRate.txt" del "Stats\EthMinerNvidia_*_HashRate.txt"
-if exist "Stats\EthminerAmd_*_HashRate.txt" del "Stats\EthMinerAmd_*_HashRate.txt"
+if exist "Stats\BMiner_*_HashRate.txt" del "Stats\BMiner_*_HashRate.txt"
+if exist "Stats\CpuminerJayddee*_HashRate.txt" del "Stats\CpuminerJayddee*_HashRate.txt"
+if exist "Stats\CcminerKlaust_*_HashRate.txt" del "Stats\CcminerKlaust_*_HashRate.txt"
 
 ECHO You need to rebenchmark some algorithms.
 PAUSE
